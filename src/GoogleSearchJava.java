68e5c3805342039995435124ee96e6e72444e604
ashutosh bindal <ashutoshbindal@gmail.com>
2017-05-13 19:44:58 +0530

    for each template this entire code works with one problem of space detection

--- src/GoogleSearchJava.java
+++ src/GoogleSearchJava.java
@@ -5,14 +5,30 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class GoogleSearchJava {
 
+    //these store the links and span
+    public static ArrayList<String> link_data = new ArrayList<String>();
+    public static ArrayList<String> span_data = new ArrayList<String>();
 
     public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
     public static void main(String[] args) throws IOException {
+
+        //text file for output
+        String FILENAME = "filename.txt";
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+        fw = new FileWriter(FILENAME);
+        bw = new BufferedWriter(fw);
+
         //Taking search term input from console
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the search term.");
@@ -20,6 +36,7 @@ public class GoogleSearchJava {
         System.out.println("Please enter the number of results:");
         int num = scanner.nextInt();
         scanner.close();
+
 
         String searchURL = GOOGLE_SEARCH_URL + "?q="+searchTerm+"&num="+num;
         //without proper User-Agent, we will get 403 error
@@ -36,19 +53,92 @@ public class GoogleSearchJava {
         for (Element result : results) {
             String linkHref = result.attr("href");
             String linkText = result.text();
-            System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&")));
+            link_data.add(linkHref.substring(7, linkHref.indexOf("&")));
+            System.out.println("Text::" + linkText + ", URL::" + linkHref.substring(7, linkHref.indexOf("&")));
 
-
-            Document doc1 = Jsoup.connect(linkHref.substring(7, linkHref.indexOf("&"))).userAgent("Mozilla/5.0").get();
-            Elements results_temp = doc1.select("title");
-            for(Element results_temp1 : results_temp) {
-                System.out.println( "Found:: " + results_temp1.text());
-            }
         }
 
         for (Element result_1: results_1) {
+            span_data.add(result_1.text());
             System.out.println("Span:" + result_1.text());
         }
+
+        //now interation for individual searches
+
+        for(int i=0; i<link_data.size();i++)
+        {
+            System.out.println(i);
+            //filter span data
+            String span_temp = span_data.get(i);
+            List<String> span_data_1 = Arrays.asList(span_temp.split("\\.\\.\\."));
+
+            /*for(String span_print : span_data_1)
+            {
+                System.out.println(span_print);
+            }*/
+
+            //search
+            Document doc_1 = Jsoup.connect(link_data.get(i)).userAgent("Mozilla/5.0").get();
+            Elements para = doc_1.select("p");
+
+            for(String span_data_temp : span_data_1) {
+
+                System.out.println("Span::" + span_data_temp);
+
+                int flag = 0;
+
+                for (Element para_temp : para) {
+
+                    String para_text = para_temp.text();
+                    para_text = para_text.replaceAll("\\[(.*?)\\]", "");
+
+
+                    if(i==1 && flag==0)
+                    {
+                        System.out.println(para_text);
+                        flag = 1;
+                    }
+                    //System.out.println(para_text);
+                    if (para_text.contains(span_data_temp)) {
+                        List<String> lines = Arrays.asList(para_text.split("\\."));
+
+
+                        try {
+
+                            //String content = "This is the content to write into file\n";
+
+
+                            for (String lines_itr : lines) {
+                                System.out.println("Done:: " + lines_itr);
+                                bw.write(lines_itr);
+                                bw.write("\n");
+                            }
+                            System.out.println("Done Writing");
+
+                        } catch (IOException e) {
+
+                            e.printStackTrace();
+
+                        }
+
+                    }
+                }
+            }
+        }
+        try {
+
+            if (bw != null)
+                bw.close();
+
+            if (fw != null)
+                fw.close();
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+
+        }
+
     }
 
 }
